246001d05ad0b9408b3df0483ff4247c983f0dc6
Dimid Duchovny <dimidd@gmail.com>
2017-09-17 20:37:56 +0300

    gui: use relative path for graphics
    Signed-off-by: Dimid Duchovny <dimidd@gmail.com>

--- openjones/src/jones/general/GUIGraphics.java
+++ openjones/src/jones/general/GUIGraphics.java
@@ -19,31 +19,31 @@ public class GUIGraphics {
 
     public GUIGraphics() {
         _mapGraphics = new HashMap<>();
-        _mapGraphics.put("Low-Cost Apartment", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/lowcost.png"));
-        _mapGraphics.put("rent", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/rent.png"));
-        _mapGraphics.put("pawn", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/pawn.png"));
-        _mapGraphics.put("lowcost_bot", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/lowcost_bot2.png"));
-        _mapGraphics.put("rent_bot", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/rent_bot2.png"));
-        _mapGraphics.put("pawn_bot", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/pawn_bot.png"));
-        _mapGraphics.put("zmarket", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/zmart.png"));
-        _mapGraphics.put("Monolith Burgers", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/monolith.png"));
-        _mapGraphics.put("QT clothing", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/clothing.png"));
-        _mapGraphics.put("clothing_left", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/clothing_left.png"));
-        _mapGraphics.put("socket_top", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/socket_top.png"));
-        _mapGraphics.put("SocketCity", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/socket_bot.png"));
-        _mapGraphics.put("HI-TECH U", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/hitech_bot.png"));
-        _mapGraphics.put("employment", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/employment_bot.png"));
-        _mapGraphics.put("factory", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/factory_bot.png"));
-        _mapGraphics.put("bank", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/bank_bot.png"));
-        _mapGraphics.put("black_top", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/black_top.png"));
-        _mapGraphics.put("black_right", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/black_right.png"));
-        _mapGraphics.put("black market", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/black_bot.png"));
-        _mapGraphics.put("Security Apartment", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/security.png"));
-        _mapGraphics.put("clock_bot", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/clock_bot.png"));
-        _mapGraphics.put("clock_top", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/clock_top3.png"));
-        _mapGraphics.put("employment_top", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/employment_top.png"));
-        _mapGraphics.put("hitech_top", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images/hitech_top3.png"));
-        _mapGraphics.put("Wall", new javax.swing.ImageIcon("/home/dimid/NetBeansProjects/openjones/openjones/images//Grass_small_blur.png"));
+        _mapGraphics.put("Low-Cost Apartment", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/lowcost.png"));
+        _mapGraphics.put("rent", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/rent.png"));
+        _mapGraphics.put("pawn", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/pawn.png"));
+        _mapGraphics.put("lowcost_bot", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/lowcost_bot2.png"));
+        _mapGraphics.put("rent_bot", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/rent_bot2.png"));
+        _mapGraphics.put("pawn_bot", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/pawn_bot.png"));
+        _mapGraphics.put("zmarket", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/zmart.png"));
+        _mapGraphics.put("Monolith Burgers", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/monolith.png"));
+        _mapGraphics.put("QT clothing", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/clothing.png"));
+        _mapGraphics.put("clothing_left", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/clothing_left.png"));
+        _mapGraphics.put("socket_top", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/socket_top.png"));
+        _mapGraphics.put("SocketCity", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/socket_bot.png"));
+        _mapGraphics.put("HI-TECH U", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/hitech_bot.png"));
+        _mapGraphics.put("employment", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/employment_bot.png"));
+        _mapGraphics.put("factory", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/factory_bot.png"));
+        _mapGraphics.put("bank", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/bank_bot.png"));
+        _mapGraphics.put("black_top", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/black_top.png"));
+        _mapGraphics.put("black_right", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/black_right.png"));
+        _mapGraphics.put("black market", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/black_bot.png"));
+        _mapGraphics.put("Security Apartment", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/security.png"));
+        _mapGraphics.put("clock_bot", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/clock_bot.png"));
+        _mapGraphics.put("clock_top", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/clock_top3.png"));
+        _mapGraphics.put("employment_top", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/employment_top.png"));
+        _mapGraphics.put("hitech_top", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/hitech_top3.png"));
+        _mapGraphics.put("Wall", new javax.swing.ImageIcon(System.getProperty("user.dir") + "/images/Grass_small_blur.png"));
 
 
         for (int row = 1; row <= 3; ++row) {
@@ -56,7 +56,7 @@ public class GUIGraphics {
                 } else {
                     str = new String("test" + index);
                 }
-                String filename = "/home/dimid/NetBeansProjects/openjones/openjones/images/" + str + ".png";
+                String filename = System.getProperty("user.dir") + "/images/" + str + ".png";
                 ImageIcon imageIcon = new javax.swing.ImageIcon(filename);
                 _mapGraphics.put(str, imageIcon);
             }
